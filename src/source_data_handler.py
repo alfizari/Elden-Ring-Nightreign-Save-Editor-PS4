312bc03c213175b10c171f8c09d06e3399709973
ip1259 <ip1259@gmail.com>
2025-12-29 20:39:50 +0800

    新增 SourceDataHandler 類別以處理遺物和效果數據，包含讀取和合併相關文本資料的邏輯

---
+++ src/source_data_handler.py
@@ -0,0 +1,165 @@
+import pandas as pd
+import pathlib
+from typing import Optional
+
+
+COLOR_MAP = ["Red", "Blue", "Yellow", "Green"]
+
+
+class SourceDataHandler:
+    WORKING_DIR = pathlib.Path(__file__).parent.resolve()
+    PARAM_DIR = pathlib.Path(WORKING_DIR / "Resources/Param")
+    TEXT_DIR = pathlib.Path(WORKING_DIR / "Resources/Text")
+    RELIC_TEXT_FILE_NAME = ["AntiqueName.fmg.xml", "AntiqueName_dlc01.fmg.xml"]
+    EFFECT_NAME_FILE_NAMES = [
+        "AttachEffectName.fmg.xml",
+        "AttachEffectName_dlc01.fmg.xml",
+    ]
+
+    def __init__(self, language: str = "en-us"):
+        self.effect_params = pd.read_csv(self.PARAM_DIR / "AttachEffectParam.csv")
+        self.effect_params: pd.DataFrame = self.effect_params[
+            ["ID", "compatibilityId", "attachTextId", "overrideEffectId"]
+        ]
+        self.effect_params.set_index("ID", inplace=True)
+
+        self.effect_table = pd.read_csv(self.PARAM_DIR / "AttachEffectTableParam.csv")
+        self.effect_table: pd.DataFrame = self.effect_table[["ID", "attachEffectId"]]
+
+        self.relic_table = pd.read_csv(self.PARAM_DIR / "EquipParamAntique.csv")
+        self.relic_table: pd.DataFrame = self.relic_table[
+            [
+                "ID",
+                "relicColor",
+                "attachEffectTableId_1",
+                "attachEffectTableId_2",
+                "attachEffectTableId_3",
+                "attachEffectTableId_curse1",
+                "attachEffectTableId_curse2",
+                "attachEffectTableId_curse3",
+            ]
+        ]
+        self.relic_table.set_index("ID", inplace=True)
+
+        self.relic_name: Optional[pd.DataFrame] = None
+        self.effect_name: Optional[pd.DataFrame] = None
+        self._load_text(language)
+
+    def _load_text(self, language: str = "en-us"):
+        language_dir_list = []
+        # search and list subfolders name that are supported language
+        for lang_dir in SourceDataHandler.TEXT_DIR.iterdir():
+            if lang_dir.is_dir():
+                language_dir_list.append(lang_dir.name)
+        _lng = language
+        if language not in language_dir_list:
+            _lng = "en-us"
+        # Deal with Relic text
+        # Read all Relic xml from language subfolder
+        _relic_names: Optional[pd.DataFrame] = None
+        for file_name in SourceDataHandler.RELIC_TEXT_FILE_NAME:
+            _df = pd.read_xml(
+                SourceDataHandler.TEXT_DIR / _lng / file_name, xpath="/fmg/entries/text"
+            )
+            if _relic_names is None:
+                _relic_names = _df
+            else:
+                _relic_names = pd.concat([_relic_names, _df])
+
+        # Deal with Effect text
+        # Read all Effect xml from language subfolder
+        _effect_names: Optional[pd.DataFrame] = None
+        for file_name in SourceDataHandler.EFFECT_NAME_FILE_NAMES:
+            _df = pd.read_xml(
+                SourceDataHandler.TEXT_DIR / _lng / file_name, xpath="/fmg/entries/text"
+            )
+            if _effect_names is None:
+                _effect_names = _df
+            else:
+                _effect_names = pd.concat([_effect_names, _df])
+        self.relic_name = _relic_names
+        self.effect_name = _effect_names
+
+    def get_relic_origin_structure(self):
+        if self.relic_name is None:
+            self._load_text()
+        _copy_df = self.relic_name.copy()
+        _copy_df.set_index("id", inplace=True)
+        _copy_df.rename(columns={"text": "name"}, inplace=True)
+        _result = {}
+        for index, row in self.relic_table.iterrows():
+            try:
+                _result[str(index)] = {
+                    "name": _copy_df.loc[index, "name"],
+                    "color": COLOR_MAP[int(self.relic_table.loc[index, "relicColor"])],
+                }
+            except KeyError:
+                _result[str(index)] = {"name": "Unset", "color": "Red"}
+        return _result
+
+    def get_relic_datas(self):
+        if self.relic_name is None:
+            self._load_text()
+        _name_map = self.relic_name.copy()
+        _name_map.reset_index(inplace=True, drop=True)
+        _name_map.rename(columns={"text": "name"}, inplace=True)
+        _result = self.relic_table.copy()
+        _result.reset_index(inplace=True)
+        _result = pd.merge(
+            _result,
+            _name_map,
+            how="left",
+            left_on="ID",
+            right_on="id",
+        )
+        _result.drop(columns=["id"], inplace=True)
+        _result.set_index("ID", inplace=True)
+        return _result
+
+    def get_effect_datas(self):
+        if self.effect_name is None:
+            self._load_text()
+        _name_map = self.effect_name.copy()
+        _name_map.reset_index(inplace=True, drop=True)
+        _name_map.rename(columns={"text": "name"}, inplace=True)
+        _result = self.effect_params.copy()
+        _result.reset_index(inplace=True)
+        _result = pd.merge(
+            _result,
+            _name_map,
+            how="left",
+            left_on="attachTextId",
+            right_on="id",
+        )
+        _result.drop(columns=["id"], inplace=True)
+        _result.set_index("ID", inplace=True)
+        _result.fillna({"name": "Unknown"}, inplace=True)
+        return _result
+
+    def get_effect_origin_structure(self):
+        if self.effect_name is None:
+            self._load_text()
+        _copy_df = self.effect_name.copy()
+        _copy_df.set_index("id", inplace=True)
+        _reslut = {}
+        for index, row in self.effect_params.iterrows():
+            try:
+                _attachTextId = self.effect_params.loc[index, "attachTextId"]
+                _reslut[str(index)] = {
+                    "name": _copy_df.loc[
+                        _attachTextId, "text"
+                    ]
+                }
+            except KeyError:
+                _reslut[str(index)] = {"name": "Unknown"}
+        return _reslut
+
+
+if __name__ == "__main__":
+    source_data_handler = SourceDataHandler("zh-tw")
+    relic_datas = source_data_handler.get_relic_datas()
+    effect_datas = source_data_handler.get_effect_datas()
+    relic_datas.to_csv(pathlib.Path.cwd() / "relic_datas.csv",
+                       encoding="utf-8")
+    effect_datas.to_csv(pathlib.Path.cwd() / "effect_datas.csv",
+                        encoding="utf-8")
